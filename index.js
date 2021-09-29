b1a5ec5b6d5224766c0e2b67af15dd7af049a5df
Your Name <Your Github Email Address>
2021-09-29 14:53:50 +0000

    worked on code challenge

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 //1
 function countToTen(){
-  
+  for (let i = 1; i <= 10; i++) {
+   console.log(i);   
+  }
 }
+countToTen();
 
 //2
-function countFromOne(){
-  
+function countFromOne(number){
+  for (let i =1; i <= number; i++){
+      console.log(i);
+  }
 }
+countFromOne(5);
 
 //3
-function countEveryEven(){
-  
+function countEveryEven(num){
+  for (let i = 2; i <= num; i +=2) {
+      console.log(i);
+  }
 }
+countEveryEven(10);
 
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(num){
+  for (let i = 1; i <= num; i +=2) {
+      console.log(i);
+  }
 }
+countEveryOdd(15);
 
 //5 
-function countEvens(){
-  
+function countEvens(numbers){
+  let evenCount = 0;
+  for (let i = 0; i < numbers.length; i++){
+      if (numbers[i] % 2 === 0){
+         evenCount += 1;
+      }
+      return evenCount;
+    }
 }
+console.log(countEvens([1,2,3,4,5,6,7,8,9,12]))
 
